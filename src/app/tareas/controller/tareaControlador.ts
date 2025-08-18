7e796f976507dbf57cdd20157c6c4a629d2859b0
Cesar Chavez <cesarelectr@hotmail.com>
2025-08-18 12:33:18 -0500

    Estandarizacion de fechas frontend and backend

--- src/app/tareas/controller/tareaControlador.ts
+++ src/app/tareas/controller/tareaControlador.ts
@@ -8,8 +8,8 @@ class TareaController {
 
 
   public async mostrarTareas(req: CustomRequest, res: Response): Promise<void> {
-    
-    const codUsuario =req.usuario.codUsuario;
+
+    const codUsuario = req.usuario.codUsuario;
 
 
     try {
@@ -21,7 +21,7 @@ class TareaController {
         tarea.fecha = MetodosLogic.formatoFecha(tarea.fecha);
         return tarea;
       });
-      
+
       res.status(200).json({ tareas });
     } catch (error) {
       console.error(error);
@@ -36,18 +36,18 @@ class TareaController {
     const date = req.body.fecha;
     const state = req.body.estado;
 
-    const codUser=req.usuario.codUsuario;
-    
-    const parametros = [name, date, state,codUser];
+    const codUser = req.usuario.codUsuario;
+
+    const parametros = [name, date, state, codUser];
 
 
     try {
       const resultado = await tareasDao.crearTareaDao(parametros);
-      res.status(200).json({ Respuesta: `La Tarea con nombre  ${resultado[0].nombre} fue registrada con exito`,mensaje:1 });
+      res.status(200).json({ Respuesta: `La Tarea con nombre  ${resultado[0].nombre} fue registrada con exito`, mensaje: 1 });
 
     } catch (error) {
       console.error(error);
-      res.status(400).json({ error: "error al crear el registro",mensaje:0 });
+      res.status(400).json({ error: "error al crear el registro", mensaje: 0 });
     }
 
   }
@@ -57,11 +57,11 @@ class TareaController {
       const codTarea = Number(req.params.codTarea);
       const nombreTarea = await tareasDao.eliminartareaDao(codTarea);
 
-      res.status(200).json({ Message: 'Tarea eliminada con éxito desde el back', Name: nombreTarea.nombre,respuesta:1});
+      res.status(200).json({ Message: 'Tarea eliminada con éxito desde el back', Name: nombreTarea.nombre, respuesta: 1 });
 
     } catch (error) {
       console.error(error);
-      res.status(400).json({ error: "error al eliminar la Tarea",respuesta:0 });
+      res.status(400).json({ error: "error al eliminar la Tarea", respuesta: 0 });
     }
 
   }
@@ -71,21 +71,22 @@ class TareaController {
     const fecha = req.body.fecha;
     const estado = req.body.estado;
     const codTarea = req.body.codTarea;
-    console.log("cod tarea en controller ",codTarea);
+    console.log("cod tarea en controller ", codTarea);
+    console.log("fecha en controller ", fecha);
     const parametros = [nombre, fecha, estado, codTarea];
     try {
       await tareasDao.actualizarTareaDao(parametros);
-      res.status(200).json({ mensaje: `Se actualizo la tarea ${nombre}`,respuesta:1 });
+      res.status(200).json({ mensaje: `Se actualizo la tarea ${nombre}`, respuesta: 1 });
 
     } catch (error) {
       console.error(error);
-      res.status(400).json({ error: "error al actualizar la Tarea",respuesta:0 });
+      res.status(400).json({ error: "error al actualizar la Tarea", respuesta: 0 });
     }
 
   }
 
-  public async notificaciones(req: CustomRequest, res: Response){
-    const codUser= req.usuario.codUsuario;
+  public async notificaciones(req: CustomRequest, res: Response) {
+    const codUser = req.usuario.codUsuario;
     try {
       const registros = await tareasDao.notificacionesDao(codUser);
       res.status(200).json(registros);
